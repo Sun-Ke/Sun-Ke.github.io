ce833945047b3cd8185f8c418d7682d6c04215af
forever_you <573570584@qq.com>
2024-02-07 22:32:56 +0800

    Site updated: 2024-02-07 22:32:55

--- show/tree/sketch.js
+++ show/tree/sketch.js
@@ -1,190 +1,192 @@
-let edges;
-let tree;
-let scale_p, size_p, number_p, style_p, space_p;
-let scale_slider, size_slider, space_slider;
-let inputbox;
-let saveJPG_button, run_button;
-let canvas,drawing;
-let number_radio,style_radio;
-let mp,rev;
+let canvas, drawing;
 let number_dx = 0, number_dy = 0;
+
+function Tree(root, edges, map, rev) {
+    this.root = root;
+    this.edges = edges;
+    this.map = map;
+    this.rev = rev;
+};
+let tree;
+
+let parameters = {
+    scale: 0.6,
+    size: 0.4,
+    space: 0.4,
+    value: 42,
+    number_on: true,
+    revert: true,
+    color: 0,
+    pixel_density: 1,
+    style: "layered",
+    root: "3",
+    text: "(1 ,2), (1, 3), (2,4), (2,5), (3,6)",
+    save_button: save_image,
+    render: process_input,
+};
+
 function setup() {
-    canvas = createCanvas(1280, 720);
+    let gui = new dat.GUI();
+    let control_folder = gui.addFolder('control');
+    let vis_folder = gui.addFolder('vis');
+    let text_folder = gui.addFolder('input');
+    let save_folder = gui.addFolder('save');
+    control_folder.add(parameters, 'scale', 0, 1).name('scale');
+    control_folder.add(parameters, 'size', 0, 1).name('size');
+    control_folder.add(parameters, 'space', 0, 1).name('space');
+    control_folder.add(parameters, 'number_on').name('number');
+
+    control_folder.add(parameters, 'style', ["layered", "radial"]).name('style');
+
+    vis_folder.add(parameters, 'color', 0, 1).name('color');
+    vis_folder.add(parameters, 'revert').name('revert');
+    vis_folder.add(parameters, 'pixel_density', 1, 3).name('pixel density');
+
+    text_folder.add(parameters, "root").name('root');
+    text_folder.add(parameters, "text").name('tree');
+    text_folder.add(parameters, "render").name('render');
+
+    save_folder.add(parameters, 'save_button').name('save as jpg');
+
+
+    control_folder.open();
+    vis_folder.open();
+    text_folder.open();
+    save_folder.open();
+
+    canvas = createCanvas(windowWidth, windowHeight)
+    canvas.style('display', 'block')
     canvas.parent('sketch');
-    drawing = createGraphics(1280, 720);
+
+    drawing = createGraphics(windowWidth, windowHeight);
     drawing.clear();
-    
-    scale_p = createP('scale');
-    scale_p.position(10, -10-2);
-    
-    size_p = createP('size');
-    size_p.position(10, 20-2);
-    
-    space_p = createP('space');
-    space_p.position(10, 50-2);
-  
-    number_p = createP('number');
-    number_p.position(10, 80-2);
-    
-    style_p = createP('style');
-    style_p.position(10, 110-2);
-    
-    
-    scale_slider = createSlider(50, 720, 720);
-    scale_slider.position(60, 10);
-    scale_slider.style('width', '140px');
-    
-    size_slider = createSlider(10, 80, 35);
-    size_slider.position(60, 40);
-    size_slider.style('width', '140px');
-    
-    space_slider = createSlider(0, 100, 50);
-    space_slider.position(60, 70);
-    space_slider.style('width', '140px');
-    
-    number_radio = createRadio('number')
-    number_radio.position(90,100)
-    number_radio.option('on');
-    number_radio.option('off');
-    number_radio.selected('off');
-  
-    style_radio = createRadio('style')
-    style_radio.position(53,130)
-    style_radio.option('layered');
-    style_radio.option('radial');
-    style_radio.selected('radial');
-  
-    saveJPG_button = createButton('save as jpg')
-    saveJPG_button.position(10, 160);
-    saveJPG_button.mousePressed(saveJPG);
-    
-  
-    let title = createElement('h1','Tree Visualization');
-    let title2 = createElement('h3','Input n and root — the number of vertices in the tree and the root number (optional). Each of the next n-1 lines contains u and v, denoting an edge connecting vertex u and vertex v. Click \'render\' to see what it looks like. You must ensure that the input data forms a tree. Have fun!');
-    
-    inputbox = createElement('textarea','6 3\n1 2\n1 3\n2 4\n2 5\n3 6');
-    inputbox.attribute('rows','7');
-    inputbox.attribute('cols','15');
-    inputbox.style('font-size','1.6em');
-    
-    run_button = createButton('render');
-    run_button.style('width','87.28px');
-    run_button.style('height','33.6px');
-    run_button.style('font-size','21.3333px');
-    run_button.mousePressed(render);
-  
-    render();
-}
-function keyPressed(){
-    if (keyCode === LEFT_ARROW){
-        number_dx -= 0.2;
-    } else if(keyCode === RIGHT_ARROW){
-        number_dx += 0.2;
-    } else if(keyCode === UP_ARROW){
-        number_dy -= 0.2;
-    } else if(keyCode === DOWN_ARROW){
-        number_dy += 0.2;
-    }
+
+    process_input();
 }
 
-function render(){
-    let alls = inputbox.value().trim().split('\n');
-    let cp = new Map();
-    let firstline = alls[0].trim().split(/\s+/);
-    let n = parseInt(firstline[0]);
-    if(n != alls.length || firstline.length > 2){
-        alert('input error!');
-        return;
-    }
+// function keyPressed() {
+//     if (keyCode === LEFT_ARROW) {
+//         number_dx -= 0.2;
+//     } else if (keyCode === RIGHT_ARROW) {
+//         number_dx += 0.2;
+//     } else if (keyCode === UP_ARROW) {
+//         number_dy -= 0.2;
+//     } else if (keyCode === DOWN_ARROW) {
+//         number_dy += 0.2;
+//     }
+// }
+
+function process_input() {
+    let alls = parameters.text;
+    let matches = alls.match(/\(.*?,.*?\)/g);
+    let n = matches.length + 1;
+    let root = parameters.root;
+    let map = new Map();
     let tot = 0;
-    if (firstline.length == 2)
-        cp.set(firstline[1],tot++);
-    for(let i = 1; i < n; i++){
-        let line = alls[i].trim().split(/\s+/);
-        if(2 != line.length){
+    map.set(root, tot++);
+    let arr = [];
+    for (let ele of matches) {
+        let pair = ele.slice(1, ele.length - 1);
+        let line = pair.split(",");
+        if (2 != line.length) {
             alert('input error!');
             return;
         }
-        if(!cp.has(line[0])){
-            cp.set(line[0],tot++);
+        let x = line[0].trim();
+        let y = line[1].trim();
+        if (!map.has(x)) {
+            map.set(x, tot++);
         }
-        if(!cp.has(line[1])){
-            cp.set(line[1],tot++);
-        } 
+        if (!map.has(y)) {
+            map.set(y, tot++);
+        }
+        arr.push([map.get(x), map.get(y)]);
     }
-    if(tot != n){
+    if (tot != n) {
         alert('input error!');
         return;
     }
     let fa = new Array(n);
-    for(let i = 0 ; i < n; i++){
+    for (let i = 0; i < n; i++) {
         fa[i] = i;
     }
-    function getfa(x){
-        if(x == fa[x]) return x;
+    function getfa(x) {
+        if (x == fa[x]) return x;
         fa[x] = getfa(fa[x])
         return fa[x];
     }
-    function merge(x,y){
+    function merge(x, y) {
         x = getfa(x);
         y = getfa(y);
-        if(x == y){
+        if (x == y) {
             return false;
         }
         fa[x] = y;
         return true;
     }
-    for(let i = 1; i < n; i++){
-        let line = alls[i].trim().split(/\s+/);
-        if(!merge(cp.get(line[0]),cp.get(line[1]))){
+
+    for (let [x, y] of arr) {
+        if (!merge(x, y)) {
             alert('input error!');
             return;
         }
     }
+
     edges = new Array(n);
-    for(let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         edges[i] = [];
     }
-    for(let i = 1; i < n; i++){
-        let line = alls[i].trim().split(/\s+/);
-        let x = cp.get(line[0]), y = cp.get(line[1]);
+    for (let [x, y] of arr) {
         edges[y].push(x);
         edges[x].push(y);
     }
-    mp = cp; //need deep copy?
     rev = new Array(n);
-    for(let [key,value] of mp){
+    for (let [key, value] of map) {
         rev[value] = key;
     }
-    return;
+    tree = new Tree(root, edges, map, rev);
 }
 
-function saveJPG(){
+function save_image() {
     saveCanvas(drawing, 'tree', 'jpg');
 }
+
 function draw() {
-    drawing.background(0);
-    let type = style_radio.value();
-    let space = space_slider.value();
-    let scale = scale_slider.value();
-    let size = size_slider.value();
-    let number_on = number_radio.value();
-    if (type == 'radial') {
-        tree = new RadialTree(edges,map(space,0,100,0,2*Math.PI));
-    } else {
-        tree = new LayeredTree(edges,map(space,0,100,0.2,3.5), map(size,10,80,0.05,2.5));
+    drawing.pixelDensity(parameters.pixel_density);
+    let color = map(parameters.color, 0, 1, 0, 255);
+    let background_color = color;
+    let stroke_color = 255;
+    if (parameters.revert) {
+        [background_color, stroke_color] = [stroke_color, background_color];
     }
-    let pos = [...tree.getPos()];
-    let n = edges.length;
+    drawing.background(background_color);
+    let style_type = parameters.style;
+    let space = map(parameters.space, 0, 1, 0, 100);
+    let scale = map(parameters.scale, 0, 1, 50, max(windowWidth, windowHeight));
+    let size = map(parameters.size, 0, 1, 20, 150);
+    let number_on = parameters.number_on;
+    let vis_tree;
+    switch (style_type) {
+        case "radial":
+            vis_tree = new RadialTree(tree.edges, map(space, 0, 100, 0, 2 * Math.PI));
+            break;
+        case "layered":
+            vis_tree = new LayeredTree(tree.edges, map(space, 0, 100, 0.2, 3.5), map(size, 10, 80, 0.05, 2.5));
+            break;
+        default:
+            alert("undefined");
+    }
+    let pos = [...vis_tree.getPos()];
+    let n = tree.edges.length;
     let cx = 0, cy = 0;
-    for(let i = 0 ; i < n; i++){
+    for (let i = 0; i < n; i++) {
         cx += pos[i].x;
         cy += pos[i].y;
     }
     cx /= n;
     cy /= n;
     let x_min = Infinity, x_max = -Infinity, y_min = Infinity, y_max = -Infinity;
-    for(let i = 0 ; i < n; i++){
+    for (let i = 0; i < n; i++) {
         pos[i].x -= cx;
         pos[i].y -= cy;
         x_min = min(x_min, pos[i].x);
@@ -193,38 +195,44 @@ function draw() {
         y_max = max(y_max, pos[i].y);
     }
     let ylen = y_max - y_min + x_max - x_min;
-    for(let i = 0 ; i < n; i++){
-        drawing.stroke(255);
+    for (let i = 0; i < n; i++) {
+        drawing.stroke(stroke_color);
         drawing.strokeWeight(3);
         drawing.noFill();
-        pos[i].x = ((x_max-x_min > 0.001)?map(pos[i].x, x_min, x_max, x_min / ylen * scale, x_max / ylen * scale): pos[i].x) + width / 2;
-        pos[i].y = ((y_max-y_min > 0.001)?map(pos[i].y, y_min, y_max, y_min / ylen * scale, y_max / ylen * scale): pos[i].y) + height / 2 + height / 12;
+        pos[i].x = ((x_max - x_min > 0.001) ? map(pos[i].x, x_min, x_max, x_min / ylen * scale, x_max / ylen * scale) : pos[i].x) + width / 2;
+        pos[i].y = ((y_max - y_min > 0.001) ? map(pos[i].y, y_min, y_max, y_min / ylen * scale, y_max / ylen * scale) : pos[i].y) + height / 2 + height / 12;
         drawing.circle(pos[i].x, pos[i].y, size);
-        if('on' == number_on){
+        if (number_on) {
             drawing.textAlign(CENTER, CENTER);
-            drawing.textSize(map(size,10,80,10,50));
+            drawing.textSize(size / 5 * 3);
             drawing.noStroke();
-            drawing.fill(255);
-            drawing.text(rev[i], pos[i].x - 0.5 + number_dx, pos[i].y + 2.5 + number_dy);
-        }     
+            drawing.fill(stroke_color);
+            drawing.text(tree.rev[i], pos[i].x - 0.5 + number_dx, pos[i].y + 2.5 + number_dy);
+        }
     }
-    for(let i = 0 ; i < n; i++){
-        for(let j of edges[i]){
+    for (let i = 0; i < n; i++) {
+        for (let j of tree.edges[i]) {
             let vx = pos[j].x - pos[i].x;
             let vy = pos[j].y - pos[i].y;
-            let vlen = Math.sqrt(vx**2+vy**2);
-            if(vlen < size)
+            let vlen = Math.sqrt(vx ** 2 + vy ** 2);
+            if (vlen < size)
                 continue;
             vx /= vlen;
             vy /= vlen;
-            drawing.stroke(255);
+            drawing.stroke(stroke_color);
             drawing.strokeWeight(3);
-            drawing.line(pos[i].x + vx * (size/2 + 10),
-                 pos[i].y + vy * (size/2 + 10),
-                 pos[j].x - vx * (size/2 + 10),
-                 pos[j].y - vy * (size/2 + 10),
-                )
+            drawing.line(pos[i].x + vx * (size / 2 + 10),
+                pos[i].y + vy * (size / 2 + 10),
+                pos[j].x - vx * (size / 2 + 10),
+                pos[j].y - vy * (size / 2 + 10),
+            )
         }
     }
-    image(drawing,0,0);
+    image(drawing, 0, 0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    drawing = createGraphics(windowWidth, windowHeight);
+    drawing.clear();
 }
--- show/tree/tree.js
+++ show/tree/tree.js
@@ -1,5 +1,5 @@
-class RadialTree{
-    constructor(edges, scope = 2*Math.PI, space = 0.8){
+class RadialTree {
+    constructor(edges, scope = 2 * Math.PI, space = 0.8) {
         this.edges = [...edges];
         this.scope = scope;
         this.space = space;
@@ -9,59 +9,59 @@ class RadialTree{
         this.angle = new Array(this.n);
         this.alpha = new Array(this.n);
         this.pos = new Array(this.n);
-        for(let i = 0; i < this.n; i++){
+        for (let i = 0; i < this.n; i++) {
             this.leaves[i] = 0;
             this.dep[i] = 0;
             this.angle[i] = 0;
             this.alpha[i] = 0;
             this.pos[i] = {
-                x : 0,
-                y : 0,
+                x: 0,
+                y: 0,
             };
         }
-        
+
         this.dfs_pre(0, -1);
         this.angle[0] = this.scope;
         this.dfs(0, -1);
     }
-    getPos() {  
+    getPos() {
         return this.pos;
     }
-    dfs_pre(u, fa){
+    dfs_pre(u, fa) {
         let sz = 0;
-        for(const v of this.edges[u]){
-            if(v == fa)
+        for (const v of this.edges[u]) {
+            if (v == fa)
                 continue;
             this.dep[v] = this.dep[u] + 1;
             this.dfs_pre(v, u);
             this.leaves[u] += this.leaves[v];
             sz++;
         }
-        if(sz == 0){
-            this.leaves[u] +=1;
+        if (sz == 0) {
+            this.leaves[u] += 1;
         }
     }
-    dfs(u, fa){
+    dfs(u, fa) {
         var sum = this.alpha[u];
-        for(const v of this.edges[u]){
-            if(v == fa)
+        for (const v of this.edges[u]) {
+            if (v == fa)
                 continue;
             this.angle[v] = this.leaves[v] / this.leaves[0] * this.scope;
             this.alpha[v] = sum;
             let alpha_uv = this.alpha[v] + this.angle[v] / 2;
-            let l = 0.0, r=100.0;
-            for(let i = 0; i < 32; i++){
+            let l = 0.0, r = 100.0;
+            for (let i = 0; i < 32; i++) {
                 let mid = (l + r) / 2;
                 this.pos[v].x = this.pos[u].x + mid * Math.cos(alpha_uv);
-                this.pos[v].y = this.pos[u].y + mid * Math.sin(alpha_uv);  
-                if(this.pos[v].x ** 2 + this.pos[v].y ** 2 < this.space*this.dep[v]) {
-                    l=mid;
+                this.pos[v].y = this.pos[u].y + mid * Math.sin(alpha_uv);
+                if (this.pos[v].x ** 2 + this.pos[v].y ** 2 < this.space * this.dep[v]) {
+                    l = mid;
                 } else {
-                    r=mid;
+                    r = mid;
                 }
             }
             this.pos[v].x = this.pos[u].x + l * Math.cos(alpha_uv);
-            this.pos[v].y = this.pos[u].y + l * Math.sin(alpha_uv); 
+            this.pos[v].y = this.pos[u].y + l * Math.sin(alpha_uv);
             sum += this.angle[v];
             this.dfs(v, u);
         }
@@ -69,8 +69,8 @@ class RadialTree{
 }
 
 
-class LayeredTree{
-    constructor(edges,sibling_separation = 1, node_size = 0.55, subtree_separation = 1.1, level_separation = 1.8){
+class LayeredTree {
+    constructor(edges, sibling_separation = 1, node_size = 0.55, subtree_separation = 1.1, level_separation = 1.8) {
         this.edges = [...edges];
         this.sibling_separation = sibling_separation;
         this.subtree_separation = subtree_separation;
@@ -87,7 +87,7 @@ class LayeredTree{
         this.rightSibling = new Array(this.n);
         this.leftNeighbor = new Array(this.n);
         this.tmp = new Array(this.n);
-        for(let i = 0; i < this.n; i++){
+        for (let i = 0; i < this.n; i++) {
             this.dep[i] = 0;
             this.size[i] = 0;
             this.parent[i] = -1;
@@ -98,10 +98,10 @@ class LayeredTree{
             this.prelim[i] = 0;
             this.modifier[i] = 0;
             this.pos[i] = {
-                x : 0,
-                y : 0,
+                x: 0,
+                y: 0,
             };
-           
+
         }
         this.dfs(0, -1, 0);
         this.firstWalk(0, 0);
@@ -109,28 +109,28 @@ class LayeredTree{
         this.yTopAdjustment = this.pos[0].y;
         this.secondWalk(0, 0, 0);
     }
-    getPos() {  
+    getPos() {
         return this.pos;
     }
-    dfs(x, fa, level){
+    dfs(x, fa, level) {
         this.size[x] += 1;
         this.dep[x] = level;
         this.leftNeighbor[x] = this.tmp[level];
         this.tmp[level] = x;
         let index = this.edges[x].indexOf(fa);
-        if (index > -1){
+        if (index > -1) {
             this.edges[x].splice(index, 1);
         }
-        for(let i = 0; i< this.edges[x].length; i++){
+        for (let i = 0; i < this.edges[x].length; i++) {
             let y = this.edges[x][i];
             this.dfs(y, x, level + 1);
             this.size[x] += this.size[y];
             this.parent[y] = x;
             if (i > 0) {
-                this.leftSibling[y] = this.edges[x][i-1];
+                this.leftSibling[y] = this.edges[x][i - 1];
             }
-            if(i + 1 < this.edges[x].length) {
-                this.rightSibling[y] = this.edges[x][i+1];
+            if (i + 1 < this.edges[x].length) {
+                this.rightSibling[y] = this.edges[x][i + 1];
             }
         }
     }
@@ -143,14 +143,14 @@ class LayeredTree{
         if (size[cur] == 1) {
             return -1;
         }
-        for(let child of edges[cur]) {
+        for (let child of edges[cur]) {
             let temp = this.getLeftMost(child, level + 1, depth);
             if (temp != -1)
                 return temp;
         }
         return -1;
     }
-    apportion(cur){
+    apportion(cur) {
         let edges = this.edges;
         let size = this.size;
         let parent = this.parent;
@@ -165,7 +165,7 @@ class LayeredTree{
             let leftModsum = 0, rightModsum = 0;
             let ancestorLeftmost = leftmost;
             let ancestorNeighbor = neighbor;
-            for(let i = 0; i < compareDepth; i++){
+            for (let i = 0; i < compareDepth; i++) {
                 ancestorLeftmost = parent[ancestorLeftmost];
                 ancestorNeighbor = parent[ancestorNeighbor];
                 rightModsum += modifier[ancestorLeftmost];
@@ -200,7 +200,7 @@ class LayeredTree{
             neighbor = leftNeighbor[leftmost];
         }
     }
-    firstWalk(cur, level){
+    firstWalk(cur, level) {
         let size = this.size;
         let edges = this.edges;
         let leftSibling = this.leftSibling;
@@ -213,10 +213,10 @@ class LayeredTree{
                 prelim[cur] = prelim[leftSibling[cur]] + this.sibling_separation + this.node_size;
             }
         } else {
-            for(let child of edges[cur]) {
+            for (let child of edges[cur]) {
                 this.firstWalk(child, level + 1);
             }
-            let midpos = (prelim[edges[cur][0]] + prelim[edges[cur][edges[cur].length-1]]) / 2;
+            let midpos = (prelim[edges[cur][0]] + prelim[edges[cur][edges[cur].length - 1]]) / 2;
             if (leftSibling[cur] != -1) {
                 prelim[cur] = prelim[leftSibling[cur]] + this.sibling_separation + this.node_size;
                 modifier[cur] = prelim[cur] - midpos;
@@ -227,12 +227,12 @@ class LayeredTree{
             }
         }
     }
-    secondWalk(cur, level, modsum){
+    secondWalk(cur, level, modsum) {
         let pos = this.pos;
         let edges = this.edges;
         pos[cur].x = this.xTopAdjustment + this.prelim[cur] + modsum;
         pos[cur].y = this.yTopAdjustment + level * this.level_separation
-        for(let child of edges[cur])
+        for (let child of edges[cur])
             this.secondWalk(child, level + 1, modsum + this.modifier[cur]);
     }
 }
